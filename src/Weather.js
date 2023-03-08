d72f51d72f96235aa166fd1ae74c77e6fae2f636
mirandalu2020 <yuanling1007@hotmail.com>
2023-03-08 08:19:00 -0800

    error messages added

--- src/Weather.js
+++ src/Weather.js
@@ -8,26 +8,35 @@ class Weather extends React.Component{
   render() {
     let listItems = []
     try {
-    console.log(this.props.weatherData.data[0].date)
-    console.log(this.props.weatherData.data[0].description)
-    for (let i of this.props.weatherData.data) {
-      listItems.push(<><li>{i.date}</li><li>{i.description}</li></>)
-    }
-    // listItems = this.props.weatherData.data.forEach(element => 
-    //    <><li>element.date</li><li>element.description</li></>
-    //  )
-      console.log(listItems)
+    // console.log(this.props.weatherData.data[0].date)
+    // console.log(this.props.weatherData.data[0].description)
+  let idx = 0;
+     for (let i of this.props.weatherData.data) {
+       listItems.push(
+       <ul>
+       <li key={idx.toString()+'date'}>{i.date}</li>
+       <li key={idx.toString()+'description'}>{i.description}</li>
+       </ul>)
+       idx +=1;
+     }
+
+  //   const listItems = this.props.weatherData.data.map((item,idx) =>
+  //   <>
+  //   <li key={idx}>{item.date}</li>
+  //   <li key={idx}>{item.description}</li>
+  //   </>
+  // );
+  // console.log('listItems: '+ listItems)
+
     } catch (error) {
-      listItems = undefined;
-      console.log(listItems)
+      listItems.push(<p>The city you are looking for is unavailable</p>)
+      // console.log(listItems)
     }
-
-
-    
 
     return (
       <>
-      <h3>Forecast</h3><ul>{listItems}</ul>
+      <h3>Forecast</h3>
+      {listItems}
       </>
     )
   }
